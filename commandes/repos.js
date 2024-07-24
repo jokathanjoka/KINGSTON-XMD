c4e3991a4801aebe9e7080f74577d009430ff65b
joel James <165368500+jokathanjoka@users.noreply.github.com>
2024-07-24 16:22:16 +0300

    Update repos.js

--- commandes/repos.js
+++ commandes/repos.js
@@ -3,8 +3,8 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const { zokou } = require("../framework/zokou");
 
 zokou({ nomCom: "repo", catégorie:"Général", reaction: "✨", nomFichier: __filename }, async (dest, zk, commandeOptions) => {
-  const githubRepo = 'https://api.github.com/repos/joeljamestech/JOEL-MD';
-  const img = 'https://telegra.ph/file/d65e03cbad4fb1fe35228.jpg';
+  const githubRepo = 'https://api.github.com/repos/jokathanjoka/KINGSTON-XMD';
+  const img = '';
 
   try {
     const response = await fetch(githubRepo);
@@ -21,17 +21,17 @@ zokou({ nomCom: "repo", catégorie:"Général", reaction: "✨", nomFichier: __f
       const releaseDate = new Date(data.created_at).toLocaleDateString('en-GB');
       const lastUpdateDate = new Date(data.updated_at).toLocaleDateString('en-GB');
 
-      const gitdata = `HELLOW WHATSAAP USER WELCOME ᴊᴏᴇʟ ᴍᴅ SCRIPT 
-this is* *joel-md.*\n GET SESSION ID *BY*, *PAIRING CODE*  https://joelsession1-4a8c04ad2935.herokuapp.com/pair/
+      const gitdata = `HELLOW WHATSAAP USER WELCOME KINGSTIN SCRIPT 
+this is* *kingstin-md.*\n GET SESSION ID *BY*, *PAIRING CODE*  /
 
 🕷️ *REPOSITORY:* ${data.html_url}
 🕷️ *STARS:* ${repoInfo.stars}
 🕷️ *FORKS:* ${repoInfo.forks}
 🕷️ *RELEASE DATE:* ${releaseDate}
 🕷️ *UPDATE ON:* ${repoInfo.lastUpdate}
-🕷️ *OWNER:* *joel tech*
+🕷️ *OWNER:* *kingston*
 __________________________________
-            𝕡𝕠𝕨𝕖𝕣𝕖𝕕 𝕓𝕪 𝕛𝕠𝕖𝕝 𝕜𝕒𝕟𝕘'𝕠𝕞𝕒`;
+       𝕡𝕠𝕨𝕖𝕣𝕖𝕕 𝕓𝕪 𝕛𝕠𝕖𝕝 *kingston*`;
 
       await zk.sendMessage(dest, { image: { url: img }, caption: gitdata });
     } else {
